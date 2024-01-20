e96f0dec42ff4a4805d826701093f118f9982bc6
Ashley Frank <ashley.frank@ufl.edu>
2024-01-20 14:34:44 -0500

    ran npm i, added: about and contact to pages, added routes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ export default function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   )
