a825c260e998af798a9a9141bba3aabb9516fb1b
Ashley Frank <ashley.frank@ufl.edu>
2024-01-21 17:37:43 -0500

    additionl project style edits

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -6,7 +6,7 @@ export default function Project({title, description, link}) {
             <div className={styles.projectInfo}>
                 <h1 className={styles.projectName}>{title}</h1>
                 <p className={styles.projectDescription}>{description}</p>
-                <button><a href={link}>Link to Project</a></button>
+                <button className={styles.projectButton}><a href={link}>Link to Project</a></button>
                 
             </div>   
         </>
